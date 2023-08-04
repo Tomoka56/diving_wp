3f5cabf20fd517788d1db26184edc36def73e9ef
Tomoka <s.pleasure0917@gmail.com>
2023-08-04 19:06:29 +0900

    フォントfunctions.phpで読み込み

--- header.php
+++ header.php
@@ -5,11 +5,6 @@
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="format-detection" content="telephone=no" />
-  <!-- google font -->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Gotu&family=Noto+Sans+JP:wght@400;500;700&family=Noto+Serif+JP&display=swap" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet" />
   <?php wp_head(); ?>
 </head>
 
